fb8abf21d77f918ca52d6552cecaee3087e736ad
V1598 <Soul-Thida-Po>
2018-03-06 14:47:04 +0900

    checking added

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -4,5 +4,7 @@ public class Helloworld {
 	 public static void main(String []args) {
 	      System.out.println("Hello World"); // prints Hello World
 	      System.out.println("First Check"); 
+	      
+	      System.out.println("checking");
 	   }
 }
